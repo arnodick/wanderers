2552a449ebcdbf2db68d8cd1b7b6203ac5a1bb52
Ashley Pringle <ash.pringle@gmail.com>
2016-05-23 16:34:33 -0300

    fixed isFile thing, just need to add directory

--- main.lua
+++ main.lua
@@ -105,15 +105,14 @@ function love.load()
 	SFX = {}
 	local files = love.filesystem.getDirectoryItems("sfx") --get all the files+directories in sfx dir
 	for i = 1,#files do
-		--TODO: for some reason this doesn't consider .wavs to be files?
-		--if love.filesystem.isFile(files[i]) then --if it isn't a directory
-		local filedata = love.filesystem.newFileData("sound", files[i])
-		local filename = filedata:getFilename() --get the sound file's name
-		local varname = string.gsub(filename, ".wav", "")
-		if filedata:getExtension() == "wav" then --if it's a .wav, add to SFX (maybe make this flexible for other sound files ie mp3)
-			SFX[varname] = love.audio.newSource("sfx/"..filename)
+		if love.filesystem.isFile("sfx/"..files[i]) then --if it isn't a directory
+			local filedata = love.filesystem.newFileData("sound", files[i])
+			local filename = filedata:getFilename() --get the sound file's name
+			local varname = string.gsub(filename, ".wav", "")
+			if filedata:getExtension() == "wav" then --if it's a .wav, add to SFX (maybe make this flexible for other sound files ie mp3)
+				SFX[varname] = love.audio.newSource("sfx/"..filename)
+			end
 		end
-		--end
 	end
 
 	Gun = {}
@@ -178,6 +177,7 @@ end
 function love.mousepressed(x, y, button)
 	if State == Enums.title then
 		State = Enums.gameplay
+		SFX.amb:setLooping(true)
 		SFX.amb:play()
 	elseif State == Enums.gameplay then
 		if button == 1 then
